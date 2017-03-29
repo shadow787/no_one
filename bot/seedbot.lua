576bc6cfecdf4615cc0433c65550f6a7e6d42ea9
shadow787 <vorojakshojae@yahoo.com>
2017-03-29 13:07:18 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {144658773},--Sudo users                               
+    sudo_users = {123880762},--Sudo users                               
   disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -229,7 +229,7 @@ An advance Administration bot based on yagop/telegram-bot
 
 اینجا کلا پاک شده!
 اگه با سودو کار داری
-@yellowhat
+@shadow787
 ]],
     help_text_realm = [[
 Realm Commands:
